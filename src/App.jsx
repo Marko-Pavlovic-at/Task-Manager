0047f9c334630c6c19345f4b3cf43ab17128704a
Marko <markopavlo.69@gmail.com>
2026-09-14 21:55:37 +0200

    input project name renders typed in vlaue

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import { useState } from "react";
 
 function App(){
   const [projects, setProjects] = useState([]);
+  const [projectName, setProjectName] = useState("");
   function addProject(){
-    setProjects([...projects, {id:Date.now(), name:"test"}]);
+    setProjects([...projects, {id:Date.now(), name: projectName}]);
     
   }
   return(
     
     <>
     <h1>Task Manager</h1>
+    <label htmlFor="projectName">Project Name:</label>
+    <input id="projectName" name="projectName" type="text" value={projectName} 
+    onChange={e => setProjectName(e.target.value)} />
     <button 
     className="button greenBtn "
     onClick={addProject}
